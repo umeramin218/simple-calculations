e1fb88534c48028cb6e4fb8c05814e869138e3d0
umeramin218 <160252422+umeramin218@users.noreply.github.com>
2024-05-01 20:02:29 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+console.log(2 + 3); // direcy addition
+var num1 = 2;
+var num2 = 5;
+console.log(num1 + num2); // addition with the help of variable
+var numOne = 3;
+var numTwo = 2;
+console.log(numOne - numTwo); // subtraction 
+var beg = 4;
+var apple = 5;
+var totalBeg = beg * apple;
+console.log(totalBeg); // multiplication
+var totalapples = 20;
+var begs = 4;
+var eachbeg = totalBeg / begs;
+console.log(eachbeg); //division
+console.log(Math.pow(10, 2));
+var layers = 5;
+var mango = 5;
+var power = Math.pow(layers, 2);
+console.log(power); //exponentiation
+console.log(5 % 2);
+var orange = 5;
+var jar = 2;
+var remain = orange % jar;
+console.log(remain); // modulus
---
+++ main.ts
@@ -0,0 +1,40 @@
+console.log(2+3); // direcy addition
+
+let num1: number = 2;
+let num2: number = 5;
+console.log(num1+num2); // addition with the help of variable
+
+let numOne: number = 3;
+let numTwo: number = 2;
+console.log(numOne-numTwo); // subtraction 
+
+let beg: number = 4;
+let apple: number= 5;
+let totalBeg: number = beg * apple;
+console.log(totalBeg); // multiplication
+
+let totalapples: number = 20;
+let begs: number = 4;
+let eachbeg: number = totalBeg/ begs;
+console.log(eachbeg); //division
+
+console.log(10**2);
+let layers: number = 5;
+let mango: number = 5;
+let power: number = layers**2;
+console.log(power);  //exponentiation
+
+console.log(5%2);
+let orange: number = 5;
+let jar: number = 2;
+let remain: number = orange%jar;
+console.log(remain); // modulus
+
+
+
+
+
+
+
+
+
